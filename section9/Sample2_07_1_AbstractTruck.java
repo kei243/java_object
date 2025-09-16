97c7e8ecf5c5f5ecc2777f624d93aa5264aaa89e
kei243 <k.honma@miracleave.co.jp>
2025-09-16 11:02:41 +0900

    抽象メソッドと例外処理について

---
+++ section9/Sample2_07_1_AbstractTruck.java
@@ -0,0 +1,15 @@
+abstract class Sample2_07_1_AbstractTruck {
+	
+	//---抽象メソッド------------------------------------------------
+	// 仕様のみを定義した継承前提のクラス
+	
+	//acceleratorメソッド（現在の速度を上げる）
+	abstract void accelerator() ;
+	
+	//brakeメソッド（現在の速度を下げる）
+	abstract void brake() ;
+	
+	//paintメソッド（色を塗る）
+	abstract void paint( String cl ) ;
+	
+}
